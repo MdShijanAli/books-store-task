5780b92ebf48f87d77ad6aceb7eb9caadb9101a3
MdShijanAli <freelancershijan@gmail.com>
2024-10-16 01:00:11 +0600

    Add
    Filter Impleemnt

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,16 +3,20 @@ import { useEffect, useState } from "react";
 import Book from "./Book";
 import ImageLoader from "./loader/ImageLoader";
 
-export default function Books({searchData}) {
+export default function Books({ searchData }) {
   const [books, setBooks] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
+  const [bookshelves, setBookshelves] = useState([])
+  const [topic, setTopic] = useState('')
+  console.log('Topic', topic);
+
 
   useEffect(() => {
     const fetchBooks = async () => {
       try {
         setIsLoading(true)
-        const response = await axios.get(`https://gutendex.com/books/?page=${ page }${searchData ? `&search=${searchData}`: ''}`);
+        const response = await axios.get(`https://gutendex.com/books/?page=${ page }${ searchData ? `&search=${ searchData }` : '' }${ topic ? `&topic=${ topic }` : '' }`);
         console.log('response', response.data.results);
         setBooks(response.data.results);
         setIsLoading(false)
@@ -23,7 +27,7 @@ export default function Books({searchData}) {
     };
 
     fetchBooks();
-  }, [page,searchData]);
+  }, [page, searchData, topic]);
 
   let content;
   if (isLoading) {
@@ -43,8 +47,26 @@ export default function Books({searchData}) {
     content = books.map((book) => <Book key={book.id} book={book} />)
   }
 
+  useEffect(() => {
+    const bookshelvesArray = books.flatMap((book) => book.bookshelves);
+
+    // If you want unique bookshelves (remove duplicates)
+    const uniqueBookshelves = [...new Set(bookshelvesArray)];
+    setBookshelves(uniqueBookshelves)
+  }, [books]);
+
+
   return (
     <div>
+      <div className="flex justify-end mb-3">
+        <select name="" id="" className="border px-5 py-2" onChange={(e) => setTopic(e.target.value)}>
+          <option default>Select</option>
+          {
+            isLoading ? <option default>Loading...</option> :
+              bookshelves?.map((item, ind) => <option key={ind} value={item}>{item}</option>)
+          }
+        </select>
+      </div>
       <div className="grid grid-cols-4 gap-5">
         {content}
       </div>
