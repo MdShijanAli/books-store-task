9c6fc814dcbf31ce11e37c0f225f79794ded4176
MdShijanAli <freelancershijan@gmail.com>
2024-10-16 23:32:43 +0600

    Add
    Wishlist added

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,22 +1,42 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import { LoveIcon } from '../components/icons/LoveIcon';
 import Search from '../components/Search';
 
-export default function Header({setSearchData}){
-    return (
-        <div className='border-b'>
-          <div className='max-w-7xl mx-auto px-10 py-3 flex justify-between items-center'>
-          <div className='w-72 h-auto'>
-            <Link to="/"><img className='w-100 h-100' src={logo} alt="Logo" /></Link>
-          </div>
-          <div>
-            <Search setSearchData={setSearchData} />
-          </div>
-          <div>
-            <LoveIcon className="w-8 h-8" />
-          </div>
+export default function Header({ setSearchData }) {
+  
+
+  const [wishList, setWishList] = useState(0);
+
+  useEffect(() => {
+    const getWishList = () => {
+      const storedWishList = localStorage.getItem("wishLists");
+      return storedWishList ? JSON.parse(storedWishList) : [];
+    };
+
+    const wishList = getWishList()
+    setWishList(wishList?.length);
+  }, []);
+
+  return (
+    <div className='border-b'>
+      <div className='max-w-7xl mx-auto px-10 py-3 flex justify-between items-center'>
+        <div className='w-72 h-auto'>
+          <Link to="/"><img className='w-100 h-100' src={logo} alt="Logo" /></Link>
         </div>
+        <div>
+          <Search setSearchData={setSearchData} />
         </div>
-    );
+        <div className='relative'>
+          <LoveIcon className="w-8 h-8" />
+          {wishList > 0 && (
+            <span className='absolute top-0 right-0 bg-red-500 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center'>
+              {wishList}
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,17 +1,59 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { LoveFillIcon } from "./icons/LoveFillIcon";
+import { LoveIcon } from './icons/LoveIcon';
 
 export default function Book({ book }) {
   const { id, title, formats, authors } = book || {};
+  const [wish, setWish] = useState(false);
+
+  const getWishList = () => {
+    const storedWishList = localStorage.getItem("wishLists");
+    return storedWishList ? JSON.parse(storedWishList) : [];
+  };
+
+  useEffect(() => {
+    const wishList = getWishList();
+    const isWished = wishList.some((item) => item.id === book.id);
+    setWish(isWished);
+  }, [book.id]);
+
+  const handleWish = (value) => {
+    const wishList = getWishList();
+
+    if (value) {
+      const updatedWishList = [...wishList, book];
+      localStorage.setItem("wishLists", JSON.stringify(updatedWishList));
+    } else {
+      const updatedWishList = wishList.filter((item) => item.id !== book.id);
+      localStorage.setItem("wishLists", JSON.stringify(updatedWishList));
+    }
+
+    setWish(value);
+  };
+
   return (
-    <div className="border p-5">
+    <div className="border p-5 relative">
+      {
+        wish ? 
+        <LoveFillIcon 
+          onClick={() => handleWish(false)} 
+          className="absolute top-0 right-0 m-1 text-red-600 cursor-pointer" 
+        /> :
+        <LoveIcon 
+          onClick={() => handleWish(true)} 
+          className="absolute top-0 right-0 m-1 cursor-pointer" 
+        />
+      }
+      
       <div className="flex justify-center h-72">
-        <Link to={`/book/${ id }`}>
-          <img className="w-full h-full bg-cover" src={formats["image/jpeg"]} alt={ title } />
+        <Link to={`/book/${id}`}>
+          <img className="w-full h-full bg-cover" src={formats["image/jpeg"]} alt={title} />
         </Link>
       </div>
       <div className="mt-3">
         <h2 className="text-sm">ID: <span className="font-semibold">{id}</span></h2>
-        <h1 className="text-md my-1">Title: <Link to={`/book/${ id }`}><span className="font-semibold hover:text-blue-950">{title.slice(0,30)}...</span></Link></h1>
+        <h1 className="text-md my-1">Title: <Link to={`/book/${id}`}><span className="font-semibold hover:text-blue-950">{title.slice(0, 30)}...</span></Link></h1>
         <h2 className="text-sm">Author: <span className="font-semibold">{authors?.[0]?.name}</span></h2>
       </div>
     </div>
